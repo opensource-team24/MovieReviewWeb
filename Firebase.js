4c8b49376ff15019478ad598d2af0d58a64a3c3f
701b <701b.vn14a@gmail.com>
2021-11-21 23:35:06 +0900

    Setting up firebase database

---
+++ Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js'
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCOZ75JeeBShX9novowym6CL-FUFiM6e0Q',
+    authDomain: 'moviereviewweb-36e87.firebaseapp.com',
+    projectId: 'moviereviewweb-36e87',
+    storageBucket: 'moviereviewweb-36e87.appspot.com',
+    messagingSenderId: '166097667548',
+    appId: '1:166097667548:web:1ca63e9824efd736783b19',
+    measurementId: 'G-THT11DMFB1'
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
